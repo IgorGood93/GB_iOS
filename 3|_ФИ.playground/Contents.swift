a7697343b1ded00d4d988de3c41eab50e53ec32a
Igor <i.gutchinov@gmail.com>
2021-06-25 18:25:24 +0300

    решение всех заданий
    по выводу в консоль, объединению структур не понял до конца. Надо пробовать дальше

--- 3|_ФИ.playground/Contents.swift
+++ 3|_ФИ.playground/Contents.swift
@@ -3,24 +3,24 @@ import Foundation
 //
 //1. Описать несколько структур – любой легковой автомобиль SportCar и любой грузовик TrunkCar.
 
+// задаем начальный вид структур
 struct SportCar {
-    let type: BodyType
-    let engine: EngineFuel
-    var volume: Double
+    let typeCar: BodyType
     let turbo: Bool
     var power: Double
     var torque: Int
     var speed: Int
     var acceleration: Double
-    var weight: Double
 }
 
 struct TrunkCar {
-    let capacity: Capacity
+    let capacity: CapacityTrunk
     let typeTrunk: BodyTypeTrunk
-    var engine: EngineFuel
+    var volumeTrunk: Double
 }
 
+
+// вводим начальные перечисления
 enum BodyType: String {
     case roadster = "Роадстер"
     case cabrio = "Кабриолет"
@@ -33,7 +33,7 @@ enum EngineFuel: String {
     case gas = "Газ"
 }
 
-enum Capacity: String {
+enum CapacityTrunk: String {
     case smallest = "1.0 тонник"
     case small = "1.5 тонник"
     case standart = "3.0 тонник"
@@ -54,45 +54,86 @@ enum BodyTypeTrunk: String {
 //
 //2. Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, запущен ли двигатель, открыты ли окна, заполненный объем багажника.
 
+
+// создаем промежуточную типовую структуру
 struct CarIhfo {
-    var brand
+    var lenght: Double
+    var width: Double
+    var height: Double
+    var weight: Double
+    var brand: String
     var prodYear: Int
-    var volumeTrunk: Double
+    let engine: EngineFuel
+    var volumeload: Double
     var volumeBody: Double
-    let engineChek: Action.Action2
-    let windowPosition: Action
+    var engineChek: ActionEngine
+    var windowPosition: ActionWindows
     var fullTrunkVolume: Double
+    
+    // ответ на 4 вопрос
+    mutating func openWindow() {
+        self.windowPosition = .open
+    }
+    
+    mutating func closeWindow() {
+        self.windowPosition = .close
+    }
 }
 
+// соединяем две структуры с общей
+
+struct SportCarInfo {
+    let SportCar: SportCar
+    var CarIhfo: CarIhfo
+}
+
+struct TrunkCarInfo {
+    var TrunkCar: TrunkCar
+    var CarIhfo: CarIhfo
+}
 
 //
 //3. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
 
-enum Action {
+enum ActionWindows {
     case open, close
-    
-    enum Action2 {
-        case on, off
-    }
-    enum Action3 {
-        case take(cargo: Double), takeOff(cargo: Double)
-    }
 }
 
+enum ActionEngine {
+    case on, off
+}
 
-
-//car1.fullTrunkVolume = Action.Action3.takeOff(cargo: -50)
-
+enum ActionCargo {
+    case take(cargo: Double), takeOff( cargo: Double)
+}
 
 //
 //4. Добавить в структуры метод с одним аргументом типа перечисления, который будет менять свойства структуры в зависимости от действия.
 
-
+// попытался объединить в новую сруктуру с заданием методов изменения аргументов, но не работает. возможно так делать не корректно, но хочется разобраться возможно ли расширение аргументов структур на определенных этапах работ
+struct MutatingSportCarInfo {
+    var SportCarInfo: SportCarInfo
+    
+    //    mutating func openWindow() {
+    //        self.windowPosition = .open
+    //    }
+    //
+    //    mutating func closeWindow() {
+    //        self.windowPosition = .close
+    //    }
+}
 
 //
 //5. Инициализировать несколько экземпляров структур. Применить к ним различные действия.
 
+var SportCar1 = SportCarInfo(SportCar: SportCar(typeCar: .cabrio, turbo: true, power: 600, torque: 5600, speed: 320, acceleration: 4.2), CarIhfo: CarIhfo(lenght: 2860, width: 1840, height: 1180, weight: 1150, brand: "Porshe", prodYear: 2008, engine: .petrol, volumeload: 60, volumeBody: 140, engineChek: .on, windowPosition: .open, fullTrunkVolume: 40))
 
+SportCar1.CarIhfo.engineChek = .off
+SportCar1.CarIhfo.windowPosition = .close
 
+var TrunkCar2 = TrunkCarInfo(TrunkCar: TrunkCar(capacity: .small, typeTrunk: .van, volumeTrunk: 120), CarIhfo: CarIhfo(lenght: 2160, width: 1940, height: 1800, weight: 1890, brand: "Mersedes", prodYear: 2004, engine: .diesel, volumeload: 800, volumeBody: 100, engineChek: .off, windowPosition: .close, fullTrunkVolume: 600))
 //
 //6. Вывести значения свойств экземпляров в консоль.
+print(SportCar1)
+print(TrunkCar2)
+
